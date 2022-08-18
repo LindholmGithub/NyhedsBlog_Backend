9967ead435601ea6bcf8ae7c4c6858be343aec6f
madsq98-desktop <mads.q@outlook.com>
2022-08-18 13:25:46 +0200

    yeah

--- NB.WebAPI/DTO/CustomerDTO/Customer_DTO_In.cs
+++ NB.WebAPI/DTO/CustomerDTO/Customer_DTO_In.cs
@@ -4,6 +4,12 @@
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        
+        public string Address { get; set; }
+        
+        public int Zipcode { get; set; }
+        
+        public string City { get; set; }
         public string Email { get; set; }
         public int PhoneNumber { get; set; }
         public string Username { get; set; }
--- NB.WebAPI/DTO/CustomerDTO/Customer_DTO_Out.cs
+++ NB.WebAPI/DTO/CustomerDTO/Customer_DTO_Out.cs
@@ -9,6 +9,12 @@ namespace NB.WebAPI.DTO.CustomerDTO
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        
+        public string Address { get; set; }
+        
+        public int Zipcode { get; set; }
+        
+        public string City { get; set; }
         public string Email { get; set; }
         public int PhoneNumber { get; set; }
         public string Username { get; set; }
