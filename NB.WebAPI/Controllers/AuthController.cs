087f0aca1e552bc42aa584e0c2b5ea54f27d5e61
madsq98-desktop <mads.q@outlook.com>
2022-09-08 13:15:45 +0200

    yas

--- NB.WebAPI/Controllers/AuthController.cs
+++ NB.WebAPI/Controllers/AuthController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Buffers.Text;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NB.WebAPI.DTO.AuthDTO;
+using NB.WebAPI.Util;
 using NyhedsBlog_Backend.Core.IServices;
 
 namespace NB.WebAPI.Controllers
@@ -15,44 +17,139 @@ namespace NB.WebAPI.Controllers
     public class AuthController : ControllerBase
     {
         private ICustomerService _service;
+        private IUserService _adminService;
+        private BasicAuthenticationReader _authReader;
 
-        public AuthController(ICustomerService service)
+        public AuthController(ICustomerService service, IUserService adminService)
         {
             _service = service;
+            _adminService = adminService;
+            _authReader = new BasicAuthenticationReader();
         }
-        
+
+        [HttpPost("admin/validate")]
+        public ActionResult<Auth_Admin_DTO_Out> ValidateAdminBase64()
+        {
+            var username = _authReader.GetUsername(HttpContext);
+            var password = _authReader.GetPassword(HttpContext);
+            try
+            {
+                var checkUser = _adminService.Validate(username, password);
+                if (checkUser != null)
+                {
+                    var base64string =
+                        System.Convert.ToBase64String(
+                            System.Text.Encoding.UTF8.GetBytes(username + ":" + password));
+
+                    return Ok(new Auth_Admin_DTO_Out
+                    {
+                        Status = 200,
+                        Message = base64string,
+                        UserId = checkUser.Id
+                    });
+                }
+
+                return Unauthorized(new Auth_Admin_DTO_Out
+                {
+                    Status = 401,
+                    Message = "Invalid Credetials"
+                });
+            }
+            catch (InvalidDataException e)
+            {
+                return Unauthorized(new Auth_Admin_DTO_Out
+                {
+                    Status = 401,
+                    Message = "Invalid Credetials"
+                });
+            }
+        }
+
+        [HttpPost("admin")]
+        public ActionResult<Auth_Admin_DTO_Out> ValidateAdmin([FromForm] Auth_DTO_In data)
+        {
+            try
+            {
+                var checkUser = _adminService.Validate(data.Username, data.Password);
+                if (checkUser != null)
+                {
+                    var base64string =
+                        System.Convert.ToBase64String(
+                            System.Text.Encoding.UTF8.GetBytes(data.Username + ":" + data.Password));
+
+                    return Ok(new Auth_Admin_DTO_Out
+                    {
+                        Status = 200,
+                        Message = base64string,
+                        UserId = checkUser.Id
+                    });
+                }
+
+                return Unauthorized(new Auth_Admin_DTO_Out
+                {
+                    Status = 401,
+                    Message = "Invalid Credetials"
+                });
+            }
+            catch (InvalidDataException e)
+            {
+                return Unauthorized(new Auth_Admin_DTO_Out
+                {
+                    Status = 401,
+                    Message = "Invalid Credetials"
+                });
+            }
+        }
+
         [HttpPost]
         public ActionResult<Auth_DTO_Out> Validate([FromForm] Auth_DTO_In data)
         {
-            var checkUser = _service.Validate(data.Username, data.Password);
-            if (checkUser != null)
+            try
             {
-                var base64string =
-                    System.Convert.ToBase64String(
-                        System.Text.Encoding.UTF8.GetBytes(data.Username + ":" + data.Password));
-                
+                var checkUser = _service.Validate(data.Username, data.Password);
+                if (checkUser != null)
+                {
+                    var base64string =
+                        System.Convert.ToBase64String(
+                            System.Text.Encoding.UTF8.GetBytes(data.Username + ":" + data.Password));
+
+                    if (data.Redirect is {Length: > 0})
+                    {
+                        return Redirect(data.Redirect + "?status=200&userid=" + checkUser.Id + "&message=" +
+                                        base64string);
+                    }
+
+                    return Ok(new Auth_DTO_Out
+                    {
+                        Status = 200,
+                        Message = base64string
+                    });
+                }
+
                 if (data.Redirect is {Length: > 0})
                 {
-                    return Redirect(data.Redirect + "?status=200&userid="+checkUser.Id+"&message="+base64string);
+                    return Redirect(data.Redirect + "?status=401&message=invalidcredentials");
                 }
-                
-                return Ok(new Auth_DTO_Out
+
+                return Unauthorized(new Auth_DTO_Out
                 {
-                    Status = 200,
-                    Message = base64string
+                    Status = 401,
+                    Message = "Invalid Credentials"
                 });
             }
-            
-            if (data.Redirect is {Length: > 0})
+            catch (InvalidDataException e)
             {
-                return Redirect(data.Redirect + "?status=401&message=invalidcredentials");
-            }
+                if (data.Redirect is {Length: > 0})
+                {
+                    return Redirect(data.Redirect + "?status=401&message=invalidcredentials");
+                }
 
-            return Unauthorized(new Auth_DTO_Out
-            {
-                Status = 401,
-                Message = "Invalid Credentials"
-            });
+                return Unauthorized(new Auth_DTO_Out
+                {
+                    Status = 401,
+                    Message = "Invalid Credentials"
+                });
+            }
         }
     }
 }
---
+++ NB.WebAPI/DTO/AuthDTO/Auth_Admin_DTO_Out.cs
@@ -0,0 +1,11 @@
+﻿namespace NB.WebAPI.DTO.AuthDTO
+{
+    public class Auth_Admin_DTO_Out
+    {
+        public int Status { get; set; }
+        
+        public string Message { get; set; }
+        
+        public int UserId { get; set; }
+    }
+}
