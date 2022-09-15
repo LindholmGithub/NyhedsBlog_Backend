9d3e01ff6ec6d72f32c2c792f3fa99f5fd65edb0
madsq98-desktop <mads.q@outlook.com>
2022-09-15 15:03:35 +0200

    Update CustomerRepository.cs

--- NB.EFCore/Repositories/CustomerRepository.cs
+++ NB.EFCore/Repositories/CustomerRepository.cs
@@ -42,11 +42,8 @@ namespace NB.EFCore.Repositories
         public Customer Update(Customer obj)
         {
             var oldObject = GetById(obj.Id);
-            
-            if(obj.Payments != null && !(obj.Payments.Count > oldObject.Payments.Count))
-            {
-                obj.Payments = oldObject.Payments;
-            }
+
+            obj.Payments ??= oldObject.Payments;
 
             var newEntity = new CustomerEntity
             {
